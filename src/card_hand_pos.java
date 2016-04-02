1b787235f020467292ab3203ae016151a2dd768f
ErikThorsson <erikorndahl@c-24-72-248-126.ni.gigamonster.net>
2016-04-01 20:28:10 -0400

    now reads enemy player's cards and positions

--- src/card_hand_pos.java
+++ src/card_hand_pos.java
@@ -10,7 +10,10 @@ import java.util.Stack;
 public class card_hand_pos {
 	String[] hand = new String[11];
 	String[] myPlay = new String[8];
+	String[] enPlay = new String[8];
     Stack myBoardState = new Stack(); //stack on cards from my play
+    Stack enBoardState = new Stack(); //stack on cards from my play
+    
     ArrayList<String> names = new ArrayList<String>(); //list of the names of my cards in play
 
 	//will cut off the mulligan phase 
@@ -30,8 +33,13 @@ public class card_hand_pos {
 
 		System.out.print("---------------------\n");
 				
-		c.getCardsInPlay();
+		c.getCardsInPlay(1);
 		c.printMyPlay();
+		
+		System.out.print("---------------------\n");
+		
+		c.getCardsInPlay(2);
+		c.printEnPlay();
 		
 		//c.printLines();
 	}
@@ -40,15 +48,12 @@ public class card_hand_pos {
 		try(BufferedReader br = new BufferedReader(new FileReader("/Users/erikorndahl/Desktop/log.txt"))) {
 		    String line = br.readLine();
 		    while (line != null) {
-		    	if (line.contains("[name=Malfurion ") && !line.contains("zonePos=0") && line.contains("name=") && line.contains("player=1") && line.contains("zone=PLAY")
-		    			&& !line.contains("Hero") && line.contains("dstPos=")
-		    			&& !line.contains("dstPos=0") || line.contains("[name=Malfurion ") && !line.contains("zonePos=0") && line.contains("name=") && line.contains("player=1") && line.contains("zone=GRAVEYARD")
-		    			&& !line.contains("Hero") && line.contains("dstPos=")
-		    			&& !line.contains("dstPos=0")) {
-		    		System.out.println(line);
-		    	}
-		    	line = br.readLine();
+		    	if (line.contains("name=Sir") && line.contains("zone=HAND") && line.contains("pos from") || 
+		    			line.contains("name=Sir") && line.contains("zone=HAND") && line.contains("TRANSITIONING")) {  
+		    	//System.out.println(line);
 		    }
+		        line = br.readLine();
+		}
 		}
 	}
 
@@ -62,6 +67,13 @@ public class card_hand_pos {
 		
 		for(int i = 0 ; i < 8 ; i++) {
 			System.out.println(myPlay[i] + " is in position " + i);
+			}
+		}
+	
+	public void printEnPlay() {
+		
+		for(int i = 0 ; i < 8 ; i++) {
+			System.out.println(enPlay[i] + " is in position " + i);
 			}
 		
 		}
@@ -79,6 +91,17 @@ public class card_hand_pos {
 		        line = br.readLine();
 		    }
 		return turn;
+		}
+	}
+	
+	public void shiftCardsLeft(){
+		for(int i=0; i < myPlay.length; i++) {
+			if(i!= myPlay.length - 1) {
+				if(myPlay[i] == null && i != 0 && myPlay[i + 1] != null) {
+					myPlay[i] = myPlay[i+1];
+					myPlay[i+1] = null;
+				}
+			}
 		}
 	}
 	
@@ -126,26 +149,39 @@ public class card_hand_pos {
 	    	if(line.contains(lastLine) || first == true)
 	  	    	found = true;
 	    	if(found == true) {
-	    	if (line.contains("[name=") && line.contains("zone=HAND") && line.contains("pos from") 
-	    			&& !lastLine.contains("FRIENDLY PLAY")) { //if the line before contains friendly play then the card is moving into play
-
-	    		String[] nameSplit = line.split("name=");
-	    		String[] s2 = nameSplit[1].split("id");
-	    		String[] pos = line.split("-> ");
+	    	if (line.contains("[name=") && line.contains("zone=HAND") && line.contains("pos from") && !lastLine.contains("FRIENDLY PLAY") 
+	    			//)
+	    			|| line.contains("name=") && line.contains("zone=HAND") && line.contains("TRANSITIONING") && !lastLine.contains("FRIENDLY PLAY"))
+	    			{ 
+	    		
+	    		//if(line.contains("name=Sir"))
+	    				//System.out.println(line);
+	    		
+	    		String[] s2 = new String[2];
+	    		String[] nameSplit = new String[2];
+	    		String[] pos = new String[2];
+	    		nameSplit = line.split("name=");
+	    		s2 = nameSplit[1].split("id");
+	    		
+	    		if( line.contains("pos from")) {
+	    		pos = line.split("-> ");
 	    		//System.out.println(s2[0].trim() + " " + pos[1]); 
 	    		hand[Integer.parseInt(pos[1])] = s2[0].trim(); //global hand array is updated with new cards and card positions
+	    		}
 	    		
 	    		//now remove old card positions if they change or are removed from hand...
-	    		if(line.contains("FRIENDLY HAND -> FRIENDLY PLAY") && line.contains("[Zone]") || line.contains("zone=GRAVEYARD")) { //then the card is moving into play so remove from hand pos
-	    		for(int i = 0; i< hand.length; i++) {
+	    		if(line.contains("to FRIENDLY PLAY")) { //then the card is moving into play so remove from hand pos
+	    			for(int i = 0; i< hand.length; i++) {
 	    			if(hand[i] != null) {
 	    			if(hand[i].equals(s2[0].trim())) {
+	    				//System.out.println("DELETED " + s2[0].trim());
 	    				hand[i] = null;
 	    			}
 	    			}
 	    		}
 	    		}
 	    		//now change the position of card in hand if it's moved
+	    		if( line.contains("pos from")) {
 	    		for(int i = 0; i< hand.length; i++) {
 	    			//if the line moves the card one position left... then delete the card from its old position. This won't delete
 	    			//newly added cards with a duplicate before them
@@ -156,6 +192,7 @@ public class card_hand_pos {
 	    			}
 	    		}
 	    	}
+	    			}
 	    	
 	        lastLine = line;
 	    	}
@@ -165,18 +202,18 @@ public class card_hand_pos {
 	}
 }
 
-public void getCardsInPlay() throws FileNotFoundException, IOException {
+	/**Need to add shifting to cards in play in one dies...Meaning null its old position**/
+public void getCardsInPlay(int p) throws FileNotFoundException, IOException {
 	try(BufferedReader br = new BufferedReader(new FileReader("/Users/erikorndahl/Desktop/log.txt"))) {
 	    String line = br.readLine();
 	    String[] boardPosition = new String[7];
-
 	    
 	    while (line != null) {
 	    	
-	    	if (line.contains("name=") && line.contains("player=1") && line.contains("zone=PLAY")
+	    	if (line.contains("name=") && line.contains("player=" + p) && line.contains("zone=PLAY")
 	    			&& !line.contains("Hero") && line.contains("dstPos=")
 	    			&& !line.contains("dstPos=0") && !line.contains("zonePos=0") || line.contains("name=") 
-	    			&& line.contains("player=1") && line.contains("zone=GRAVEYARD")
+	    			&& line.contains("player=" + p) && line.contains("zone=GRAVEYARD")
 	    			&& !line.contains("Hero") && line.contains("dstPos=") && !line.contains("zonePos=0") 
 	    			&& !line.contains("dstPos=0"))  {
     			
@@ -212,35 +249,55 @@ public void getCardsInPlay() throws FileNotFoundException, IOException {
 	    		//System.out.println(line + "_____" + cardName);
 	    		
 	    		//check to see if it was destroyed
-	    		if(line.contains("TO_BE_DESTROYED") || line.contains("zone=GRAVEYARD")) {
+	    		if(line.contains("TO_BE_DESTROYED") || line.contains("zone=GRAVEYARD") && !line.contains("tag=NUM_ATTACKS_THIS_TURN")) {
 	    			destroyed.add(cardName);
-	    			//System.out.println(cardName);
+	    			//System.out.println(cardName + " DESTROYED");
 	    		}
 	    		
 	    		//add name to card position
-	    	    if(pos != -1) {
+	    	    if(pos != -1 && !line.contains("GRAVEYARD")) {
 		    	    //System.out.println("ADDING CARD TO PLAY " + cardName + " at position " + pos);
-	    	    	myPlay[pos] = cardName;
+		    	    
+		    	    //if the card is shifting left. Might cancel adding cards before other cards....don;t know the output yet
+		    	    if(myPlay[pos+1] == cardName) {
+		    	    	myPlay[pos+1] = null;
+		    	    }
+	    	    	if(p==1)
+	    	    		myPlay[pos] = cardName;
+	    	    	else
+	    	    		enPlay[pos] = cardName;
 	    	    }
 	    		   	    	    	
 	    	   //now check to see if any cards were destroyed and eliminate their array position
+	    	    if(p == 1) {
 	    	   for(int j = 0; j < myPlay.length; j++) {
 	    		   for(int i = 0 ; i < destroyed.size(); i++) {
 	    	    	if(myPlay[j] != null) {
 	    			   if(myPlay[j].equals(destroyed.get(i))) {
 	    		    	    //System.out.println("REMOVING CARD" + myPlay[j]);
-	    				   myPlay[j] = null;
+	    	    			System.out.println("Removing " + myPlay[j]); 
+	    				    myPlay[j] = null;
 	    	    			destroyed.remove(i); //remove the destroyed card from destroyed...
-
 	    			   }
 	    	    	}
 	    	    	}
 	    	    }
-	    	   
-	    	    //myBoardState.push(nameAndPos);
-	    		//System.out.println(cardName + " " + position2[0]);
-	    		//myPlay[Integer.parseInt(position2[0].trim())] = cardName;
+	    	} else {
+	    		for(int j = 0; j < enPlay.length; j++) {
+	    		   for(int i = 0 ; i < destroyed.size(); i++) {
+		    	    	if(enPlay[j] != null) {
+		    			   if(enPlay[j].equals(destroyed.get(i))) {
+		    				    enPlay[j] = null;
+		    	    			destroyed.remove(i); //remove the destroyed card from destroyed...
+
+		    			   }
+		    	    	}
+		    	    	}
+		    	    }
 	    	}
+	    	    //shifts cards to the left if one is killed to its left
+	    	    shiftCardsLeft();
+	    }
 	    	
 	        line = br.readLine();
 	    }
